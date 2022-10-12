26df04e30da5f15ecae767805fe175c842793e6d
Roman Hatz <roman.hatz@srf.ch>
2022-10-12 23:43:21 +0200

    ..

--- adminer/index.php
+++ adminer/index.php
@@ -1,5 +1,13 @@
 <?php
-
-echo "test"; 
+/*******************************************************
+** Adminer, since version 4.7.0 does not accept        *
+** connections without a password.                     *
+** For version 4.7.x to accept an empty password,      *
+** in the adminer-4.7.x.php file, replace :            *
+** login($Ae,$F){if($F=="") by login($Ae,$F){if(1===2) *
+** This can be done automatically by replacing false   *
+** with true in the line below.                        *
+*******************************************************/
+include "adminer-4.7.6.php"
 
 ?>
